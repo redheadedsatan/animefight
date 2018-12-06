12e29d53bc7b4ca28c379874f06f9fb2429c4dfe
yoyo421 <yoavram2000@gmail.com>
2018-12-07 00:16:55 +0200

    fix

---
+++ src/main/java/com/anime/fight/Class/Ranger.java
@@ -0,0 +1,22 @@
+package com.anime.fight.Class;
+
+import com.anime.fight.Class.Base.FighterBase;
+import lombok.Getter;
+
+
+public class Ranger extends FighterBase {
+
+    @Getter
+    protected double abiltDmgMod;
+    /**
+     * shoots an arrow with lower dmg and move away from the target
+     */
+    public void ability(FighterBase target) {
+        target.defend(baseDamage * abiltDmgMod);
+        int x,y;
+        x = 2*(position.getX() - target.position.getX())/(Math.abs(position.getX() - target.position.getX()));
+        y = 2*(position.getY() - target.position.getY())/(Math.abs(position.getY() - target.position.getY()));
+
+
+    }
+}
