2fb5bdb8d963e7dce836a4121e811749bb16ba3d
redheadedsatan <misha.zelikson@gmail.com>
2018-12-06 19:05:10 +0200

    # This is a combination of 4 commits. # This is the 1st commit message:
    init app
    
    # This is the commit message #2:
    
    aa
    
    # This is the commit message #3:
    
    test
    
    # This is the commit message #4:
    
    start things out

---
+++ src/main/java/com/anime/fight/Class/Beserker.java
@@ -0,0 +1,55 @@
+package com.anime.fight.Class;
+
+import com.anime.fight.Class.Base.FighterBase;
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+public class Beserker extends FighterBase {
+
+    @Getter
+    protected double SelfDmgMinimum = 2;
+
+    public void ability(FighterBase target) {
+        double selfDmg = hpMax*0.05;
+        if (SelfDmgMinimum > selfDmg) { selfDmg = SelfDmgMinimum; }
+        hp -= selfDmg;
+        if (hp <= 0)
+        {
+            hp = 1;
+        }
+        basicAttack(target);
+        basicAttack(target);
+    }
+    public FighterBase findCloseEnemy(FighterBase[] enemy)
+    {
+        FighterBase close= targets[0];
+        int minDistence = position.distance(targets[0].position);
+        for (int i = 1; i < targets.length;i++) {
+            if (minDistence > position.distance(targets[i].position)) {
+                minDistence = position.distance(targets[i].position);
+                close = targets[i];
+            }
+        }
+        return close;
+    }
+    public void logic(FighterBase[] targets) {
+        FighterBase target = findCloseEnemy(targets);
+        double diss = position.distance(target.position);
+        if (diss <= baseRange && (System.currentTimeMillis() - lastAttackTime) == attackSpeed)
+        {
+            if (System.currentTimeMillis() - lastAbilltyTime == abiltyColdown) {
+                ability(target);
+                lastAbilltyTime = System.currentTimeMillis();
+                lastAttackTime = System.currentTimeMillis();
+            }
+            else {
+                basicAttack(target);
+                lastAttackTime = System.currentTimeMillis();
+            }
+        }
+        else {
+                
+            }
+
+
+    }
+}
