bed908fc3a8eec80db323511829239fd4f9bc971
yoyo421 <yoavram2000@gmail.com>
2018-12-07 02:06:16 +0200

    midwork

--- src/main/java/com/anime/fight/Class/Beserker.java
+++ src/main/java/com/anime/fight/Class/Beserker.java
@@ -1,45 +1,45 @@
-package com.anime.fight.Class;
-
-import com.anime.fight.Class.Base.FighterBase;
-import lombok.Getter;
-
-
-public class Beserker extends FighterBase {
-
-    @Getter
-    protected double SelfDmgMinimum = 2;
-
-    public void ability(FighterBase target) {
-        double selfDmg = hpMax*0.05;
-        if (SelfDmgMinimum > selfDmg) { selfDmg = SelfDmgMinimum; }
-        hp -= selfDmg;
-        if (hp <= 0)
-        {
-            hp = 1;
-        }
-        basicAttack(target);
-        basicAttack(target);
-    }
-
-    public void logic(FighterBase[] targets) {
-        FighterBase target = findCloseEnemy(targets);
-        double diss = position.distance(target);
-        if (diss <= baseRange && (System.currentTimeMillis() - lastAttackTime) == attackSpeed)
-        {
-            if (System.currentTimeMillis() - lastAbilltyTime == abiltyColdown) {
-                ability(target);
-                lastAbilltyTime = System.currentTimeMillis();
-                lastAttackTime = System.currentTimeMillis();
-            }
-            else {
-                basicAttack(target);
-                lastAttackTime = System.currentTimeMillis();
-            }
-        }
-        else {
-
-            }
-
-
-    }
-}
+//package com.anime.fight.Class;
+//
+//import com.anime.fight.Class.Base.FighterBase;
+//import lombok.Getter;
+//
+//
+//public class Beserker extends FighterBase {
+//
+//    @Getter
+//    protected double SelfDmgMinimum = 2;
+//
+//    public void ability(FighterBase target) {
+//        double selfDmg = hpMax*0.05;
+//        if (SelfDmgMinimum > selfDmg) { selfDmg = SelfDmgMinimum; }
+//        hp -= selfDmg;
+//        if (hp <= 0)
+//        {
+//            hp = 1;
+//        }
+//        basicAttack(target);
+//        basicAttack(target);
+//    }
+//
+//    public void logic(FighterBase[] targets) {
+//        FighterBase target = findCloseEnemy(targets);
+//        double diss = position.distance(target);
+//        if (diss <= baseRange && (System.currentTimeMillis() - lastAttackTime) == attackSpeed)
+//        {
+//            if (System.currentTimeMillis() - lastAbilltyTime == abiltyColdown) {
+//                ability(target);
+//                lastAbilltyTime = System.currentTimeMillis();
+//                lastAttackTime = System.currentTimeMillis();
+//            }
+//            else {
+//                basicAttack(target);
+//                lastAttackTime = System.currentTimeMillis();
+//            }
+//        }
+//        else {
+//
+//            }
+//
+//
+//    }
+//}
--- src/main/java/com/anime/fight/Main.java
+++ src/main/java/com/anime/fight/Main.java
@@ -4,13 +4,39 @@ import com.anime.fight.UserInterface.Camera;
 import com.anime.fight.event.BasicEvents;
 import org.reflections.Reflections;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IllegalAccessException, InstantiationException {
-        BasicEvents a = new BasicEvents() {
+        /*BasicEvents a = new BasicEvents() {
 
         };
-        a.OnFrame();
+        a.OnFrame();*/
+
+        List<BasicEvents> listeners = new ArrayList<BasicEvents>();
+
+        Reflections reflections = new Reflections("com.anime.fight");
+        Set<Class<? extends BasicEvents>> classes = reflections.getSubTypesOf(BasicEvents.class);
+        for (Class<? extends BasicEvents> aClass : classes)
+        {
+            listeners.add(aClass);
+            for (Method m : aClass.getMethods())
+            {
+                for (Method a : BasicEvents.class.getMethods())
+                {
+                    if (m.getName().equals(a.getName())) {
+                        try {
+                            m.invoke(m, null);
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+                        System.out.println(m.getName());
+                    }
+                }
+            }
+        }
     }
 }
